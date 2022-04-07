23eb6d000c20a1f6cd8c340cab366494f028c37a
bdanielbs <dansakalabs@gmail.com>
2022-04-07 13:52:11 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 *Return: 0 after printing funtions
 */
 int main(void) {
-puts("/"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
