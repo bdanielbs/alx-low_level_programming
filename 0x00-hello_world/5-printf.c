c89c66fce71f7a5ac99ac64e0eb1511137042f65
bdanielbs <dansakalabs@gmail.com>
2022-04-07 14:03:38 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *my hello, printf task
+*Return:return 1 after programme execution
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
