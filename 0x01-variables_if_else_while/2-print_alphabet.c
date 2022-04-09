b907514c0ec36073f5dabf0ca78a2679f914dbe3
bdanielbs <dansakalabs@gmail.com>
2022-04-09 08:20:58 -0700

    c2task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+*
+*Return:Always 0 (success/correct)
+*/
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alpphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+}
