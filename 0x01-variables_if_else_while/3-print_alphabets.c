d6c8bfdfc6442d60821cf25f0b5025b6b63e1a7e
bdanielbs <dansakalabs@gmail.com>
2022-04-09 09:02:29 -0700

    c2task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,13 +13,13 @@ for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 
-putchar('\n');
-
 }
 
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
 putchar(upper_case);
+
+putchar('\n');
 }
 
 return (0);
