ecce294980a9271c2a9c99a045890fc1294be829
bdanielbs <dansakalabs@gmail.com>
2022-04-10 03:33:24 -0700

    c2task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+*
+*Return:Always 0 (success/correct)
+*/
+
+int main(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+putchar('\n');
+
+return (0);
