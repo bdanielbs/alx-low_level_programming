22fe0312ebf094238a7bd1cd565a0a2c8d9bcf09
bdanielbs <dansakalabs@gmail.com>
2022-04-10 04:02:29 -0700

    c2task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return:Always 0 (success/correct)
+*/
+
+int main(void)
+{
+int d;
+
+for (d = 0; d < 10; d++)
+{
+printf("%d", d);
+}
+printf("\n");
+
+return (0);
+}
